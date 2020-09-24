f0e0d20a97c07c8a1e3f98707360c8111ca250c6
aodehnalova <aodehnalova@A222-08>
2020-09-24 13:56:42 +0200

    Zmena Andrea

--- pokus30/Form1.cs
+++ pokus30/Form1.cs
@@ -16,5 +16,10 @@ namespace pokus30
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Zmena Andrea
+        }
     }
 }
