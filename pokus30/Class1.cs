99ec48e68d60e29dbbd3913536e24010e322de4f
aodehnalova <aodehnalova@A222-08>
2020-09-24 14:07:37 +0200

    trida

---
+++ pokus30/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokus30
+{
+    class Class1
+    {
+        //moje trida
+    }
+}
